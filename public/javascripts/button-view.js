a62dc8a399e5ec20c4ff09dba770ab54cb4f9084
gunner <gdhatmaker@gmail.com>
2020-05-18 17:11:25 -0500

    defense fix

--- public/javascripts/button-view.js
+++ public/javascripts/button-view.js
@@ -4,17 +4,21 @@ $(document).ready(function () {
         $(".qb-full").show();
         $(".rb-full").show();
         $(".wr-full").show();
+        $(".df-full").hide();
+
     });
     $("#show-te-button").click(function () {
         $(".te-full").show();
         $(".qb-full").hide();
         $(".rb-full").hide();
         $(".wr-full").hide();
+        $(".df-full").hide();
     });
     $("#show-qb-button").click(function () {
         $(".qb-full").show();
         $(".te-full").hide();
         $(".rb-full").hide();
+        $(".df-full").hide();
         $(".wr-full").hide();
     });
     $("#show-wr-button").click(function () {
@@ -22,17 +26,31 @@ $(document).ready(function () {
         $(".te-full").hide();
         $(".rb-full").hide();
         $(".qb-full").hide();
+        $(".df-full").hide();
+
     });
     $("#show-rb-button").click(function () {
         $(".rb-full").show();
         $(".te-full").hide();
         $(".qb-full").hide();
         $(".wr-full").hide();
+        $(".df-full").hide();
+
     });
     $("#show-qb-button").click(function () {
         $(".qb-full").show();
         $(".te-full").hide();
         $(".rb-full").hide();
         $(".wr-full").hide();
+        $(".df-full").hide();
+
+    });
+    $("#show-df-button").click(function () {
+        $(".df-full").show();
+        $(".te-full").hide();
+        $(".rb-full").hide();
+        $(".wr-full").hide();
+        $(".qb-full").hide();
+
     });
 });
